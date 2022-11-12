8ce0b434ba87cd1d90026fa52508a763d83363ed
simplex3510 <leeyj6457@naver.com>
2022-11-12 23:19:37 +0900

    캐릭터 생성 창에서의 스탯 분배 구현
    데이터 저장 및 관리 매니저 활용 필요

--- Assets/Scripts/UI/StartGameScene/StatDistribute.cs
+++ Assets/Scripts/UI/StartGameScene/StatDistribute.cs
@@ -9,12 +9,18 @@ public class StatDistribute : MonoBehaviour
     public TMP_Text statsText;
     public TMP_Text sociabilityText;
     public TMP_Text learningAbilityText;
+    public TMP_Text economicAbilityText;
+    public TMP_Text specAbilityText;
     public Slider sociabilitySlider;
     public Slider learningAbilitySlider;
+    public Slider economicAbilitySlider;
+    public Slider specAbilitySlider;
 
-    static int stats = 12;
-    static int sociability = 0;
-    static int learningAbility = 0;
+    private int stats = 12;
+    private int sociability = 0;
+    private int learningAbility = 0;
+    private int economicAbility = 0;
+    private int specAbility = 0;
 
     #region sociability
     public void OnClickPlusSociability()
@@ -111,6 +117,106 @@ public class StatDistribute : MonoBehaviour
         {
             timer += Time.deltaTime;
             learningAbilitySlider.value = Mathf.Lerp(learningAbilitySlider.value, destination, timer);
+            yield return null;
+        }
+    }
+    #endregion
+
+    #region EconomicAbility
+    public void OnClickPlusEconomicAbility()
+    {
+        if (economicAbility < 10 && 0 < stats)
+        {
+            stats--;
+            economicAbility++;
+        }
+
+        if (stats <= 0)
+        {
+            stats = 0;
+        }
+
+        StartCoroutine(ChangeEconomicAbility());
+        economicAbilityText.text = economicAbility.ToString();
+        statsText.text = stats.ToString();
+    }
+
+    public void OnClickMinusEconomicAbility()
+    {
+        if (0 < economicAbility)
+        {
+            stats++;
+            economicAbility--;
+        }
+
+        if (12 <= stats)
+        {
+            stats = 12;
+        }
+
+        StartCoroutine(ChangeEconomicAbility());
+        economicAbilityText.text = economicAbility.ToString();
+        statsText.text = stats.ToString();
+    }
+
+    private IEnumerator ChangeEconomicAbility()
+    {
+        float timer = 0.0f;
+        float destination = economicAbility * 10f;
+        while (timer <= 0.3f)
+        {
+            timer += Time.deltaTime;
+            economicAbilitySlider.value = Mathf.Lerp(economicAbilitySlider.value, destination, timer);
+            yield return null;
+        }
+    }
+    #endregion
+
+    #region SpecAbility
+    public void OnClickPlusSpecAbility()
+    {
+        if (specAbility < 10 && 0 < stats)
+        {
+            stats--;
+            specAbility++;
+        }
+
+        if (stats <= 0)
+        {
+            stats = 0;
+        }
+
+        StartCoroutine(ChangeSpecAbility());
+        specAbilityText.text = specAbility.ToString();
+        statsText.text = stats.ToString();
+    }
+
+    public void OnClickMinusSpecAbility()
+    {
+        if (0 < specAbility)
+        {
+            stats++;
+            specAbility--;
+        }
+
+        if (12 <= stats)
+        {
+            stats = 12;
+        }
+
+        StartCoroutine(ChangeSpecAbility());
+        specAbilityText.text = specAbility.ToString();
+        statsText.text = stats.ToString();
+    }
+
+    private IEnumerator ChangeSpecAbility()
+    {
+        float timer = 0.0f;
+        float destination = specAbility * 10f;
+        while (timer <= 0.3f)
+        {
+            timer += Time.deltaTime;
+            specAbilitySlider.value = Mathf.Lerp(specAbilitySlider.value, destination, timer);
             yield return null;
         }
     }
